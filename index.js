a735069a59db910f6025ed397c079866ebd965e7
Sangsi-Chaitrareddy <chaitrareddy8105@gmail.com>
2026-03-03 21:19:24 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+// This function takes two numbers (a and b) and adds them
+function add(a, b) {
+  return a + b;
+}
+
+// This line allows our test file to "see" and use this function
+module.exports = add;
